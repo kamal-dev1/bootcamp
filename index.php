ac710167c23556636e992d35496921300038afc5
kamal gharejeloo <kamal.gharejeloo@outlook.com>
2022-12-30 21:55:47 +0330

    add htaccess

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-?>
+
+$request=$_SERVER['REQUEST_URI'];
+echo $_SERVER['REQUEST_URI'];
+switch($request){
+    case '/add':
+        require __DIR__ . "/add.php";
+        break;
+    case '/poll':
+        require __DIR__ . "/add.php";
+        break;
+    default:
+        require __DIR__ . "/add.php";
+}
